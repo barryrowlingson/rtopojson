b620003b47648c96a37f4b0ccac28a0d7a8aef38
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2013-12-18 15:20:11 +0000

    documented and exported

--- R/readtopojson.R
+++ R/readtopojson.R
@@ -1,15 +1,32 @@
+
 readthefile <- function(f){
     require(RJSONIO)
     return(fromJSON(f))
 }
 
-
+##' Read an object from a topojson file
+##'
+##' Reads topojson files
+##' @title read an object from a topojson file
+##' @param f topojson file name
+##' @param objectname object name
+##' @return SpatialPolygons object
+##' @author Barry S Rowlingson
+##' @export
 readObject <- function(f,objectname){
     j = readthefile(f)
     object = j$objects[[objectname]]
     decodeObjectSP(j,object)
 }
 
+##' Get the names of objects in a topojson file
+##'
+##' Get the names of objects
+##' @title Get topojson object names
+##' @param f topojson format file name
+##' @return vector of names
+##' @author Barry S Rowlingson
+##' @export
 objectNames <- function(f){
     j = readthefile(f)
     names(j$objects)
